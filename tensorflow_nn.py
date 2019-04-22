943df8af4265ed0c6bf2efd6434d798d4e639667
paulmorio <pms69@cam.ac.uk>
2019-04-22 22:03:02 +0100

    Addition of main loop and session for computational graph

--- tensorflow_nn.py
+++ tensorflow_nn.py
@@ -1,10 +1,18 @@
 """
 This is a basic pre 2.0 tensorflow version of simpleNN network class
 
+There are many guides and tutorials that say tensorflow creates a computational graph,
+and trains the network. which is entirely correct. But lack of additional words leaves 
+people in confusion around what the role of python is here in this situation.
+
+
+
 Author: Paul Scherer
 Date: April 2019
 
 """
+import numpy as np
+import random
 import tensorflow as tf
 
 ##########
@@ -13,6 +21,25 @@ import tensorflow as tf
 import mnist_loader_python3
 training_data, _, test_data = mnist_loader_python3.load_data_wrapper()
 
+# Make data usable with Tensorflow
+def create_batches(data, batch_size):
+	"""
+	Returns training data as a list of batches in format [(x_batch, y_batch)]
+	where x_batch is the batch of observations and y_batch is the batch of labels
+	in one-hot encoding if necessary
+	"""
+	batches = [training_data[k:k+batch_size] for k in range(0, len(data), batch_size)]
+	tf_compliant_batches = []
+	for batch in batches:
+		mini_batch_obs = []
+		mini_batch_labels = []
+		for obs in batch:
+			x,y = obs
+			mini_batch_obs.append(x.flatten())
+			mini_batch_labels.append(y.flatten())
+		tf_compliant_batches.append((np.array(mini_batch_obs), np.array(mini_batch_labels)))
+	return tf_compliant_batches
+
 #####################
 ## HyperParameters ##
 #####################
@@ -20,9 +47,9 @@ learning_rate = 0.001
 epochs = 500
 batch_size = 100
 
-################################
-### Network Shape Paramaters ###
-################################
+###########################
+### Network Definition  ###
+###########################
 input_dim = 784
 hidden_layer_dim = 64
 output_dim = 10
@@ -57,10 +84,41 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_fn)
 
 # Evaluation subgraph
-correct_prediction = tf.equals(tf.argmax(prediction,1), tf.argmax(y,1))
+correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initialize the variables in their start values
 init = tf.global_variables_initializer()
 
+##########################
+### Main Control Loop  ###
+##########################
+with tf.Session() as sess:
+	# Run the initializer we created to instantiate the variables
+	sess.run(init)
 
+	# The main loop
+	for epoch in range(epochs):
+		random.shuffle(training_data)
+		tf_training_data = create_batches(training_data, batch_size)
+		for mini_batch in tf_training_data:
+			x_batch, y_batch = mini_batch
+
+			# The full forward and backward pass in one go using the optimizers
+			# See that the train_op is the last thing in the computational graph,
+			# with x and y being the inputs at the start of that computational graph
+			sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
+
+		# Just some display stuff
+		if epoch % 10 == 0:
+			loss, acc = sess.run([loss_fn, accuracy], feed_dict={x: x_batch, y: y_batch})
+			print ("Epoch %s, Loss: %s, Accuracy %s" % (epoch, loss, acc))
+
+	print("## Training Finished ##")
+
+	# #############################
+	# ## Evaluation on Test Data ##
+	# #############################
+
+	# test_acc = sess.run(accuracy, feed_dict={x: test_x, test_y})
+	# print ("Test accuracy: {0}".format(test_acc))
